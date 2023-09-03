4012d70c6d45fa42917b60c6dc95351e17ba90b4
daleyjones <djones_15@hotmail.com>
2023-09-03 19:29:46 +0100

    added js files

---
+++ data/App.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import './App.css';
+
+import layout from "./components/layout";
+
+import PortfolioDashboard from "./components/PortfolioDashboard";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+const pagfes = ["portfolio", "contact", "about"];
+
+function App() {
+    const [page, setPage] = useState("portfolio"); ?
+
+  return (
+<layout pages={pages} onSetPage={setPage}>
+{page ==="portfolio" && <PortfolioDashboard />}
+{page ==="about" && <About />}
+
+{page ==="contact" && <Contact />}
+
+
+
+
+
+</layout>
+  );
+}
+
+export default App;
